a4d0f51919701e623f4c4b9b44a6d6a7da8492f6
Ilu92 <39953750+Ilu92@users.noreply.github.com>
2020-07-27 22:37:30 +0700

    Add Python Turtle (Start)
    Использую метод "черепашка" и рисую

---
+++ Start.py
@@ -0,0 +1,10 @@
+import turtle # Подключаем черепашку
+
+start = turtle.Turtle()
+# start.speed(20) # Скорость
+start.color("#7812de") # Цвет
+
+
+for i in range (5): # Цикл подключаю
+   start.left(144) # Градус и направление
+   start.forward(270) # Размер "звезды"
